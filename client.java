6eb956139edeacdb3d2e19db10ffff1e49ded126
CailanStm <cailanstm@shaw.ca>
2017-10-30 15:08:05 -0700

    Begin server code, add gitignore

--- client.java
+++ client.java
@@ -2,6 +2,16 @@
 Implement client functionality as per assignment spec
 
 */
+import java.net.*;
+import java.security.*;
+import javax.crypto.*;
 
-import java.security;
-import javax.crypto;
+public class Client {
+	public static void main(String[] args) {
+		try {
+			Socket serverConnection = new Socket("localhost", 8080);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+	}
+}
--- server.java
+++ server.java
@@ -2,6 +2,20 @@
 Implement server functionality as per assignment spec
 
 */
+import java.net.*;
+import java.security.*;
+import javax.crypto.*;
 
-import java.security;
-import javax.crypto;
+public class Server {
+	
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(8080);
+			System.out.println("Waiting for client...");
+			Socket clientConnection = server.accept();
+			System.out.println("Client connected!");					
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+	}
+}
